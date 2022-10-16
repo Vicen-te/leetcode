76d819bbc02fe5a186d5c7c402b9bfdd837276fa
Vicen-te <vicenesne@gmail.com>
2022-10-16 23:29:52 +0200

    Create 876-Middle-of-the-Linked-List.cpp

---
+++ algorithms/cpp/876-Middle-of-the-Linked-List.cpp
@@ -0,0 +1,43 @@
+// Author  :   Vicen-te
+// Date    :   09/10/2022
+
+/*
+    Given the head of a singly linked list, return the middle node of the linked list.
+    If there are two middle nodes, return the second middle node.
+
+    Ex. 
+    Input: head = [1,2,3,4,5]
+    Output: [3,4,5]
+
+    1.- Set two pointers (one takes the next Node / one by one, 
+        two takes the next Node from the Next Node / two by two)
+    2.- Check the two position or the one next to two
+    3.- Continue until the two position or the one next to two is null
+
+    Time: O(N) 
+    Space: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* one = head;
+        ListNode* two = head;
+
+        while (two != NULL && two->next != NULL) {
+            one = one->next;
+            two = two->next->next;
+        }
+        return one;
+    }
+};
